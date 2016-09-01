038bf59edd788eec178d5f2f607acfa76d3a75bf
Nigel Lee <lihui8@le.com>
2016-09-01 17:52:41 +0800

    finish table view so that it can be used to show the contend of sortednames.plist

--- Sections/Sections/ViewController.m
+++ Sections/Sections/ViewController.m
@@ -22,6 +22,11 @@ static NSString *SectionsTableIdentifier = @"SectionsTableIdentifier";
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UITableView *table = (id)[self.view viewWithTag:1];
+    [table registerClass:[UITableViewCell class] forCellReuseIdentifier:SectionsTableIdentifier];
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"sortednames" ofType:@"plist"];
+    self.names = [NSDictionary dictionaryWithContentsOfFile:path];
+    self.keys = [[self.names allKeys] sortedArrayUsingSelector:@selector(compare:)];
 }
 
 
@@ -29,6 +34,38 @@ static NSString *SectionsTableIdentifier = @"SectionsTableIdentifier";
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark-
+#pragma mark Table View Data Source Methods
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return [self.keys count];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView indentationLevelForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    NSString *key = self.keys[section];
+    NSArray *nameSection = self.names[key];
+    return [nameSection count];
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    return self.keys[section];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:SectionsTableIdentifier forIndexPath:indexPath];
+    NSString *key = self.keys[indexPath.section];
+    NSArray *nameSection = self.names[key];
+    cell.textLabel.text = nameSection[indexPath.row];
+    return cell;
+}
+
+
+
+
 
 
 @end
